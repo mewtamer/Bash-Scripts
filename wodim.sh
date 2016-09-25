c3b2c7b8b050ce78909633ba9d3417c91ccca012
Jeffery Wright <mewtamer@gmail.com>
2016-09-25 19:02:09 +0000

    Updated wodim.sh to take image to be burned as command line argument.

--- wodim.sh
+++ wodim.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-wodim -eject  -tao speed=0 dev=/dev/sr1 -v -data A761.iso
-##wodim -eject  -tao speed=1 dev=/dev/scd0 -v -data /my/directory/image.iso
+wodim -eject  -tao speed=0 dev=/dev/sr1 -v -data $1
